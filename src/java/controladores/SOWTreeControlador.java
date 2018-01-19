585b73dd9f9a721107fcba96f458772952a0436c
nmoha <nmoha@DESKTOP-EU9GE2I>
2018-01-19 11:44:47 +0100

    ULTIMA VERSION - FUNCIONANDO

--- src/java/controladores/SOWTreeControlador.java
+++ src/java/controladores/SOWTreeControlador.java
@@ -42,146 +42,142 @@ import java.io.StringWriter;
 import java.util.Iterator;
 import org.apache.log4j.Logger;
 
-
 /**
  *
  * @author nmohamed
  */
 @Controller
 public class SOWTreeControlador {
-     static Logger log = Logger.getLogger(SOWTreeControlador.class.getName());
-     private Object getBean(String nombrebean, ServletContext servlet)
-    {
+
+    static Logger log = Logger.getLogger(SOWTreeControlador.class.getName());
+
+    private Object getBean(String nombrebean, ServletContext servlet) {
         ApplicationContext contexto = WebApplicationContextUtils.getRequiredWebApplicationContext(servlet);
         Object beanobject = contexto.getBean(nombrebean);
         return beanobject;
     }
+
     @RequestMapping("/sowdisplay/start.htm")
     public ModelAndView start(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
-        if((new SessionCheck()).checkSession(hsr))
-           return new ModelAndView("redirect:/userform.htm?opcion=inicio");
-        ModelAndView mv = new ModelAndView("sowdisplay");
-    try{
-        ResultSet rs = DBConect.ah.executeQuery("SELECT GradeLevel,GradeLevelID FROM AH_ZAF.dbo.GradeLevels");
-        List <Level> grades = new ArrayList();
-        Level l = new Level();
-        l.setName("Select level");
-        grades.add(l);
-        while(rs.next())
-        {
-            Level x = new Level();
-             String[] ids = new String[1];
-             ids[0]=""+rs.getInt("GradeLevelID");
-            x.setId(ids);
-            x.setName(rs.getString("GradeLevel"));
-        grades.add(x);
+        if ((new SessionCheck()).checkSession(hsr)) {
+            return new ModelAndView("redirect:/userform.htm?opcion=inicio");
         }
-    mv.addObject("levels", grades);
-    }catch(SQLException ex){
-        StringWriter errors = new StringWriter();
-        ex.printStackTrace(new PrintWriter(errors));
-        log.error(ex+errors.toString());
+        ModelAndView mv = new ModelAndView("sowdisplay");
+        try {
+            ResultSet rs = DBConect.ah.executeQuery("SELECT GradeLevel,GradeLevelID FROM AH_ZAF.dbo.GradeLevels");
+            List<Level> grades = new ArrayList();
+            Level l = new Level();
+            l.setName("Select level");
+            grades.add(l);
+            while (rs.next()) {
+                Level x = new Level();
+                String[] ids = new String[1];
+                ids[0] = "" + rs.getInt("GradeLevelID");
+                x.setId(ids);
+                x.setName(rs.getString("GradeLevel"));
+                grades.add(x);
+            }
+            mv.addObject("levels", grades);
+        } catch (SQLException ex) {
+            StringWriter errors = new StringWriter();
+            ex.printStackTrace(new PrintWriter(errors));
+            log.error(ex + errors.toString());
+        }
+        return mv;
     }
-    return mv;
-    }
+
     @RequestMapping("/sowdisplay/loadtree.htm")
     @ResponseBody
     public String loadtree(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
-    ModelAndView mv = new ModelAndView("sowdisplay");
-     JSONObject json = new JSONObject();
-      ArrayList<DBRecords> steps = new ArrayList<>();
-      ArrayList<String> subjects = new ArrayList<>();
-      ArrayList<String> objectives = new ArrayList<>();
-       Tree tree = new Tree();
-       Node<String> rootNode = new Node<String>("Subjects","A"," {\"disabled\":true}");
-    try{
+        ModelAndView mv = new ModelAndView("sowdisplay");
+        JSONObject json = new JSONObject();
+        ArrayList<DBRecords> steps = new ArrayList<>();
+        ArrayList<String> subjects = new ArrayList<>();
+        ArrayList<String> objectives = new ArrayList<>();
+        Tree tree = new Tree();
+        Node<String> rootNode = new Node<String>("Subjects", "A", " {\"disabled\":true}");
+        try {
 
-        String[] levelid = hsr.getParameterValues("seleccion1");
-        ArrayList<Subject> subs = this.getSubjects(levelid);
+            String[] levelid = hsr.getParameterValues("seleccion1");
+            ArrayList<Subject> subs = this.getSubjects(levelid);
 
-        for(Subject sub:subs){
-            String[] sid = sub.getId();
-        ResultSet rs = DBConect.eduweb.executeQuery("select obj_steps.id,obj_steps.name,objective.name as obj ,objective.subject_id from obj_steps inner join objective on obj_steps.obj_id = objective.id where objective.subject_id = '"+sid[0]+"'");
-        
-        while(rs.next())
-        {
-            DBRecords l = new DBRecords();
-            l.setCol1(""+rs.getInt("id"));
-            l.setCol2(rs.getString("name"));
-            l.setCol4(rs.getString("obj"));
-            l.setCol3(""+rs.getInt("subject_id"));
-            if(!objectives.contains(rs.getString("obj"))){
-            objectives.add(rs.getString("obj"));
+            for (Subject sub : subs) {
+                String[] sid = sub.getId();
+                ResultSet rs = DBConect.eduweb.executeQuery("select obj_steps.id,obj_steps.name,objective.name as obj ,objective.subject_id from obj_steps inner join objective on obj_steps.obj_id = objective.id where objective.subject_id = '" + sid[0] + "'");
+
+                while (rs.next()) {
+                    DBRecords l = new DBRecords();
+                    l.setCol1("" + rs.getInt("id"));
+                    l.setCol2(rs.getString("name"));
+                    l.setCol4(rs.getString("obj"));
+                    l.setCol3("" + rs.getInt("subject_id"));
+                    if (!objectives.contains(rs.getString("obj"))) {
+                        objectives.add(rs.getString("obj"));
+                    }
+                    steps.add(l);
+
+                }
             }
-            steps.add(l); 
-          
-        }
-        }
-        for (DBRecords x :steps)
-        {
-            Subject s = new Subject();
-            String id = null;
-            id = x.getCol3();
-            x.setCol3(s.fetchName(Integer.parseInt(id), hsr.getServletContext()));
-            if(!subjects.contains(x.getCol3())){
-            subjects.add(x.getCol3());
+            for (DBRecords x : steps) {
+                Subject s = new Subject();
+                String id = null;
+                id = x.getCol3();
+                x.setCol3(s.fetchName(Integer.parseInt(id), hsr.getServletContext()));
+                if (!subjects.contains(x.getCol3())) {
+                    subjects.add(x.getCol3());
+                }
             }
-        }
-  
-   
-    String test = new Gson().toJson(steps);
-   
-    
-    int i = 0;
-   int z = 0;
-    for(Subject x:subs)//subjects)
-    {
-         
-        Node<String> nodeC = new Node<String>(x.getName(),"L"+i," {\"disabled\":true}");
-        rootNode.addChild(nodeC); 
-      i++;
-      ArrayList<Objective> obj = this.getObjectives(x.getId());
-         for(Objective y:obj)
-    {
-    
-    Node<String> nodeA = new Node<String>(y.getName(),"C"+z," {\"disabled\":true}");
-             nodeC.addChild(nodeA);
-         z++;
-     for (DBRecords l:steps){
-         
-         //match the subject with the objective
-         if(l.getCol3().equalsIgnoreCase(x.getName())&&l.getCol4().equalsIgnoreCase(y.getName()))
-         {
-           
-         //match the objective with the step
-       for (DBRecords k:steps){
-          if(k.getCol4().equalsIgnoreCase(y.getName())){
-        Node<String> nodeB = new Node<String>(k.getCol2(),k.getCol1()," {\"disabled\":false}");
-      
-         nodeA.addChild(nodeB);
-          }
-       }
-       break;
-         }
-        
-        }
-    }
-        
-    }
-   
-     }catch (SQLException ex) {
+
+            String test = new Gson().toJson(steps);
+
+            int i = 0;
+            int z = 0;
+            for (Subject x : subs)//subjects)
+            {
+
+                Node<String> nodeC = new Node<String>(x.getName(), "L" + i, " {\"disabled\":true}");
+                rootNode.addChild(nodeC);
+                i++;
+                ArrayList<Objective> obj = this.getObjectives(x.getId());
+                for (Objective y : obj) {
+
+                    Node<String> nodeA = new Node<String>(y.getName(), "C" + z, " {\"disabled\":true}");
+                    nodeC.addChild(nodeA);
+                    z++;
+                    for (DBRecords l : steps) {
+
+                        //match the subject with the objective
+                        if (l.getCol3().equalsIgnoreCase(x.getName()) && l.getCol4().equalsIgnoreCase(y.getName())) {
+
+                            //match the objective with the step
+                            for (DBRecords k : steps) {
+                                if (k.getCol4().equalsIgnoreCase(y.getName())) {
+                                    Node<String> nodeB = new Node<String>(k.getCol2(), k.getCol1(), " {\"disabled\":false}");
+
+                                    nodeA.addChild(nodeB);
+                                }
+                            }
+                            break;
+                        }
+
+                    }
+                }
+
+            }
+
+        } catch (SQLException ex) {
             System.out.println("Error: " + ex);
             StringWriter errors = new StringWriter();
             ex.printStackTrace(new PrintWriter(errors));
-            log.error(ex+errors.toString());
+            log.error(ex + errors.toString());
         }
 
         tree.setRootElement(rootNode);
         String test2 = this.generateJSONfromTree(tree);
-    
-    
-    return test2;
+
+        return test2;
     }
+
     public String generateJSONfromTree(Tree tree) throws IOException, JSONException {
         ObjectMapper mapper = new ObjectMapper();
         JsonFactory factory = new JsonFactory();
@@ -201,97 +197,87 @@ public class SOWTreeControlador {
 
         obN.put("text", node.getData());
         obN.put("id", node.getId());
-        obN.put("state",node.getState());
+        obN.put("state", node.getState());
         JSONObject j = new JSONObject();
 //        j.put("opened",true);
 //        j.put("disabled",false);
 //        obN.put("state",j.toString());
         ArrayNode childN = obN.arrayNode();
-        obN.set("children", childN);        
+        obN.set("children", childN);
         if (node.getChildren() == null || node.getChildren().isEmpty()) {
             return obN;
         }
 
         Iterator<Node<String>> it = node.getChildren().iterator();
-        while (it.hasNext()) {  
+        while (it.hasNext()) {
             childN.add(generateJSON(it.next(), new ObjectMapper().createObjectNode()));
         }
         return obN;
     }
 
-    public ArrayList<Subject> getSubjects(String[] levelid) throws SQLException
-       {
-           
+    public ArrayList<Subject> getSubjects(String[] levelid) throws SQLException {
+
         ArrayList<Subject> subjects = new ArrayList<>();
         ArrayList<Subject> activesubjects = new ArrayList<>();
-        try{
-             
-          ResultSet rs1 = DBConect.ah.executeQuery("select CourseID from Course_GradeLevel where GradeLevel IN (select GradeLevel from GradeLevels where GradeLevelID ="+levelid[0]+")");
-           Subject s = new Subject();
-          s.setName("Select Subject");
-          subjects.add(s);
-           
-           while (rs1.next())
-            {
-             Subject sub = new Subject();
-             String[] ids = new String[1];
-            ids[0]=""+rs1.getInt("CourseID");
-             sub.setId(ids);
-            
+        try {
+
+            ResultSet rs1 = DBConect.ah.executeQuery("select CourseID from Course_GradeLevel where GradeLevel IN (select GradeLevel from GradeLevels where GradeLevelID =" + levelid[0] + ")");
+            Subject s = new Subject();
+            s.setName("Select Subject");
+            subjects.add(s);
+
+            while (rs1.next()) {
+                Subject sub = new Subject();
+                String[] ids = new String[1];
+                ids[0] = "" + rs1.getInt("CourseID");
+                sub.setId(ids);
+
                 subjects.add(sub);
             }
-           for(Subject su:subjects.subList(1,subjects.size()))
-          {
-              String[] ids = new String[1];
-              ids=su.getId();
-           ResultSet rs2 = DBConect.ah.executeQuery("select Title,Active from Courses where CourseID = "+ids[0]);
-           while(rs2.next())
-           {
-            if(rs2.getBoolean("Active")== true)
-               {
-                   su.setName(rs2.getString("Title"));
-                   activesubjects.add(su);
-               }
-           }
-          }
-        }catch(SQLException ex){
-        StringWriter errors = new StringWriter();
-        ex.printStackTrace(new PrintWriter(errors));
-        log.error(ex+errors.toString());
+            for (Subject su : subjects.subList(1, subjects.size())) {
+                String[] ids = new String[1];
+                ids = su.getId();
+                ResultSet rs2 = DBConect.ah.executeQuery("select Title,Active from Courses where CourseID = " + ids[0]);
+                while (rs2.next()) {
+                    if (rs2.getBoolean("Active") == true) {
+                        su.setName(rs2.getString("Title"));
+                        activesubjects.add(su);
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            StringWriter errors = new StringWriter();
+            ex.printStackTrace(new PrintWriter(errors));
+            log.error(ex + errors.toString());
         }
-           return activesubjects;
-       }
-        public ArrayList<Objective> getObjectives(String[] subjectid) throws SQLException
-       {
-           ArrayList<Objective> objectives = new ArrayList<>();
-       try {
-            
-          ResultSet rs1 = DBConect.eduweb.executeQuery("select name,id from public.objective where subject_id="+subjectid[0]);
+        return activesubjects;
+    }
+
+    public ArrayList<Objective> getObjectives(String[] subjectid) throws SQLException {
+        ArrayList<Objective> objectives = new ArrayList<>();
+        try {
+
+            ResultSet rs1 = DBConect.eduweb.executeQuery("select name,id from public.objective where subject_id=" + subjectid[0]);
 //          Objective s = new Objective();
 //          s.setName("Select Objective");
 //          objectives.add(s);
-           
-           while (rs1.next())
-            {
-             String[] ids = new String[1];
+
+            while (rs1.next()) {
+                String[] ids = new String[1];
                 Objective sub = new Objective();
-            ids[0] = ""+rs1.getInt("id");
-             sub.setId(ids);
-             sub.setName(rs1.getString("name"));
+                ids[0] = "" + rs1.getInt("id");
+                sub.setId(ids);
+                sub.setName(rs1.getString("name"));
                 objectives.add(sub);
             }
-          
-            
+
         } catch (SQLException ex) {
             System.out.println("Error leyendo Objectives: " + ex);
             StringWriter errors = new StringWriter();
             ex.printStackTrace(new PrintWriter(errors));
-            log.error(ex+errors.toString());
+            log.error(ex + errors.toString());
         }
-       return objectives;
-       }
-       
+        return objectives;
+    }
 
-    
-}    
-
+}
